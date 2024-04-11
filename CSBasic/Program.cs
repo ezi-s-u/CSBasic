2add8ae52ce7f3be021e486a3c992bcd6b9d77b7
Jisu Lee <ljsgoo0617@gmail.com>
2024-04-11 11:40:37 +0900

    초기 커밋.

---
+++ CSBasic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic
+// namespace : 패키지 역할을 함
+{
+    internal class Program
+    // internal : 접근 제한자(생략하면 문제가 생김)
+    {
+        static void Main(string[] args)
+        //  Main의 M이 대문자, string의 s가 소문자 (java와 다름)
+        {
+            
+        }
+    }
+}
